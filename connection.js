801b9274e074b26e61a11f9293c8281424cbacc5
manavvp08 <manavp080@gmail.com>
2024-03-05 01:53:47 +0530

    first commit

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.s11qz.mongodb.net/chatAppMern?retryWrites=true&w=majority`, ()=> {
+mongoose.connect(`mongodb+srv://manavp080:manavisgood@cluster0.9ivoywi.mongodb.net/`, ()=> {
   console.log('connected to mongodb')
 })
--- models/User.js
+++ models/User.js
@@ -21,6 +21,7 @@ const UserSchema = new mongoose.Schema({
   },
   picture: {
     type: String,
+    required: false,
   },
   newMessages: {
     type: Object,
